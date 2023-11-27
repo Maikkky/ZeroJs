85c1c65329e597084ded3d3db9bc18ee7b07a5c5
Maikkky <maikarujr@gmail.com>
2023-11-27 13:43:42 +0700

    Commit Pertama Js

---
+++ main.js
@@ -0,0 +1,29 @@
+// Deklarasi variabel dengan tipe data yang jelas
+const nama = 'Mikhael'; // Tipe Data String
+const umur = 24; // Tipe Data Number
+const tabungan = 500000.1; // Tipe Data Float / Koma
+const manusia = true; // Tipe Data Boolean
+
+// Operator Aritmatika
+const a = 100;
+const b = 20;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+// Penjumlahan Tipe Number
+const uangAwal = 5000;
+const uangDitabung = 10000;
+const totalUang = uangAwal + uangDitabung;
+console.log(`Jadi Total Tabungan Saya Adalah: Rp. ${totalUang}`);
+
+// Penjumlahan String
+const namaDepan = 'Mikhael';
+const namaBelakang = 'Julian';
+
+const namaLengkap = `${namaDepan} ${namaBelakang}`;
+console.log(`${namaDepan} ${namaBelakang}`); // Cara Satu Menggabungkan 2 Variable
+console.log(namaLengkap); // Cara Dua Menggunakan Variable Yang Sudah Berisi Nilai
