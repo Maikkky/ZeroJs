4e7b8c363304c602f6eed71e3bb98505fdee071d
Maikkky <maikarujr@gmail.com>
2023-11-29 03:32:43 +0700

    looping while js

---
+++ mingguSatu/whileloop.js
@@ -0,0 +1,9 @@
+let angka = 1;
+while (angka <= 5) {
+  console.log(angka);
+  angka++;
+}
+
+/*Looping while digunakan ketika kita ingin melakukan iterasi selama kondisi tertentu terpenuhi. 
+Pada contoh di atas, kode akan terus mencetak nilai angka dari 1 hingga 5 karena kondisi angka <= 5.
+*/
